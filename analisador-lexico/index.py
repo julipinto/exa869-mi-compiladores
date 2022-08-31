08d33eb350f9866ea917eb503be2744cbc1de64c
adkatarine <adllakatarine@hotmail.com>
2022-08-30 21:53:07 -0300

    adiciona verificacao de comentarios de linha e de bloco

--- analisador-lexico/index.py
+++ analisador-lexico/index.py
@@ -1,4 +1,5 @@
 import re
+
 root = "./files/"
 demo = root + "demofile.txt"
 
@@ -57,11 +58,29 @@ def findNumber(line, first_index):
     raise Exception("Um ponto precisa ser seguido de outro número")
   return (number, last_index)
 
+def isComment(line, first_index):
+  last_index = first_index
+  line_length = len(line)
+  comment = ""
+
+  while last_index < line_length:
+    if(line[last_index] == '*'):
+      comment += line[last_index]
+      if(line[last_index+1] == '/'):
+        comment += line[last_index+1]
+        last_index += 2
+        break
+    last_index += 1
+
+  if(comment[last_index-1] != '*' and comment[-1] != '/'):
+    raise Exception("Um ponto precisa ser seguido de outro número")
+  return (comment, last_index)
+
 def isSpace(char):
   return char == " " or char == "\t"
 
 def isDelimiter(char):
-  return char in ["(", ")", ";", ",", "=", ":", ".", ">", "<", "!", "&", "|", "~", "^", "*", "/", "-", "+", " ", "\t"]
+  return char in ["(", ")", ";", ",", "=", ":", ".", ">", "<", "!", "&", "|", "~", "^", "*", "-", "+", " ", "\t"]
 
 """
 Essa função vai achar o próximo conjunto de caracteres
@@ -79,12 +98,52 @@ def findNext(linha, index):
 
   return (string, final_string)
 
+def lineComment(line, index):
+  if line[index] != '/' and line[index+1] != '/':
+    raise Exception("Não é um comentário de linha")
+
+  line_length = len(line)
+  index_end = index
+  comment = ""
+
+  while index_end < line_length:
+    comment += line[index_end]
+    index_end += 1
+
+  return (comment, index_end)
+
+def findEndBlock(line, index_start):
+  line_length = len(line)
+  index_end = index_start
+  comment = ""
+
+  while index_end < line_length:
+    comment += line[index_end]
+    if len(comment) > 1 and  comment[-2] == '*' and comment[-1] == '/':
+      return (True, comment, index_end)
+    index_end += 1
+  
+  return (False, comment, index_end)
+
+block_comment = ""
+is_comment_block = False
+
 def handleLine(linha):
+    global block_comment, is_comment_block
     line_length = len(linha)
     index = 0
     while index < line_length:
       palavra = ""
-      if linha[index] == '"':
+      if is_comment_block:
+        (achou, comment, index) = findEndBlock(linha, index)
+        is_comment_block = not achou
+        block_comment += comment
+        if achou:
+          block_comment = ""
+          print("achou o comentário \n", block_comment)
+        index += 1
+        continue
+      elif linha[index] == '"':
         (palavra, index) = findString(linha, index)
       elif isSpace(linha[index]):
         index += 1
@@ -93,6 +152,20 @@ def handleLine(linha):
         palavra = linha[index]
       elif linha[index].isnumeric():
         (palavra, index) = findNumber(linha, index)
+      elif linha[index] == '/':
+        if linha[index+1] == '/':
+          (comment, index) = lineComment(linha, index)
+        elif linha[index+1] == '*':
+          is_comment_block = True
+          continue
+        #  (achou, comment, index) = findEndBlock(linha, index)
+        #  is_comment_block = not achou
+        #  block_comment += comment
+        #if achou:
+        #  print("achou o comentário \n", block_comment)
+        else:
+          palavra = linha[index] # vai ser um operador
+
       else:
         (palavra, index) = findNext(linha, index)
         if reserved_regex.match(palavra):
